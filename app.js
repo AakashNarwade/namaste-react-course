4748bf24ea4b77abd49a7d0c6b2b592274592dc8
AakashNarwade <aakashnarwade06@gmail.com>
2023-07-26 05:46:58 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "child1h1"),
+    React.createElement("h2", {}, "child1h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "child2h1"),
+    React.createElement("h2", {}, "child2h2"),
+  ]),
+]);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
