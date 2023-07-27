c91bc17ab18b900536c94f24a5a6aed7e9d40ca5
AakashNarwade <aakashnarwade06@gmail.com>
2023-07-28 04:28:55 +0530

    Episode-02 Iginiting our app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "child1h1"),
     React.createElement("h2", {}, "child1h2"),
+    React.createElement("h2", {}, "namaste react"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "child2h1"),
