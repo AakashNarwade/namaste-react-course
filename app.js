31654f5bc11bd3e25346154cde7e361b05d6e72e
AakashNarwade <aakashnarwade06@gmail.com>
2023-07-28 04:55:54 +0530

    Episode -03 Laying the foundation

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "child1h1"),
-    React.createElement("h2", {}, "child1h2"),
-    React.createElement("h2", {}, "namaste react"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "child2h1"),
-    React.createElement("h2", {}, "child2h2"),
-  ]),
-]);
-console.log(parent);
+
+
+const Title = () =>{
+
+ return  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 
+  </h1>
+}
+
+
+
+const HeadingComponent = () =>{
+ return ( <div id="container">
+    <h1 className="heading">
+      Namaste React Fucntional Component
+    </h1>
+    <Title/>
+  </div>)
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>)
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "child1h1"),
+//     React.createElement("h2", {}, "child1h2"),
+//     React.createElement("h2", {}, "namaste react"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "child2h1"),
+//     React.createElement("h2", {}, "child2h2"),
+//   ]),
+// ]);
+// console.log(parent);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
